4ced41e64eb4dcf0eb2ca7baac650beb0e410cd1
Aswitha <114471559+ASWITHA10@users.noreply.github.com>
2023-02-28 15:54:07 +0530

    Create youtube.r

---
+++ vac assigm/web scrapping/youtube.r
@@ -0,0 +1,25 @@
+library(robotstxt)
+library(dplyr)
+library(rvest)
+url <- "https://www.tastyedits.com/top-subscribed-youtube-channels/"
+path = paths_allowed(url)
+#html from website
+web = read_html(url)
+View(web)
+
+rank <- web %>% html_nodes(".number:nth-child(1)")%>% html_text()
+View(rank)
+
+channel <- web %>%html_nodes(".text:nth-child(2)") %>% html_text()
+View(channel)
+
+subs <- web%>%html_nodes(".text+ .number") %>% html_text()
+View(subs)
+
+#creating dataframe
+youtube <- data.frame(rank,channel,subs)
+View(youtube)
+#save
+write.csv(youtube,"top_subs.csv")
+
+in
